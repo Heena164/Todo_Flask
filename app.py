fbdbec1c01f237ad1f57700314505c9b34a175d0
Heena164 <heenasr164@gmail.com>
2023-06-19 23:44:30 +0530

    Todo list

--- app.py
+++ app.py
@@ -2,20 +2,16 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-# db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
-
 
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200), nullable = False)
-    desc = db.Column(db.String(500), nullable = False)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
@@ -32,13 +28,26 @@ def hello_world():
     allTodo = Todo.query.all() 
     return render_template('index.html', allTodo=allTodo)
 
-
-
-@app.route('/update')
-def update():
+@app.route('/show')
+def products():
     allTodo = Todo.query.all()
     print(allTodo)
-    return 'this is a product page'
+    return 'this is products page'
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+        
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
@@ -47,10 +56,10 @@ def delete(sno):
     db.session.commit()
     return redirect("/")
 
-with app.app_context():
-    db.create_all()
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000) 
-
-
+    app.run(debug=True, port=8000)
